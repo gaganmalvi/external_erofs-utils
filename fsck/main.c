2689786633ef3cdd3b2fa5474aa0b50fb69bc1ed
Yue Hu <zbestahu@gmail.com>
2022-07-22 13:36:10 +0800

    erofs-utils: fix a memory leak of multiple devices
    The memory allocated for multiple devices should be freed after use.
    Let's add a helper to fix it since there is more than one to use it.
    
    Signed-off-by: Yue Hu <huyue2@coolpad.com>
    Link: https://lore.kernel.org/r/20220722053610.23912-1-huyue2@coolpad.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fsck/main.c
+++ fsck/main.c
@@ -772,7 +772,7 @@ int main(int argc, char **argv)
 
 	if (erofs_sb_has_sb_chksum() && erofs_check_sb_chksum()) {
 		erofs_err("failed to verify superblock checksum");
-		goto exit_dev_close;
+		goto exit_put_super;
 	}
 
 	err = erofsfsck_check_inode(sbi.root_nid, sbi.root_nid);
@@ -797,6 +797,8 @@ int main(int argc, char **argv)
 		}
 	}
 
+exit_put_super:
+	erofs_put_super();
 exit_dev_close:
 	dev_close();
 exit:
--- fuse/main.c
+++ fuse/main.c
@@ -294,6 +294,8 @@ int main(int argc, char *argv[])
 	}
 
 	ret = fuse_main(args.argc, args.argv, &erofs_ops, NULL);
+
+	erofs_put_super();
 err_dev_close:
 	blob_closeall();
 	dev_close();
